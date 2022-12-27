ad6050fbe1879efff7d4a820efb837730b6120d7
joseam10 <A48349@alunos.isel.pt>
2022-12-27 14:01:21 +0000

    Command_new almost functional

--- serie_3/app/app.c
+++ serie_3/app/app.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "command.h"
 
-void command_print() {
-	print_commands(commands_list)
-}
 
 int main() {
-	Commands *commands_list = malloc(sizeof *commands_list);
-	commands_list_init(commands_list);
-	command_insert(commands_list, 'h', "list available commands", );
-	(Command *)(commands_list->commands->next->data)->command();
+	char line[100];
+	commands_list_init();
+	
+	command_insert('h', "\t - Listar comandos existentes", print_commands);
+	command_insert('c', "\t - Incorporar novo comando", command_new);
+
+	while (1) {
+		putchar('>');
+		fgets(line, sizeof line, stdin);
+		char *command = strtok(line, " \n");
+		char *name = strtok(NULL, " \n");
+		if (command != NULL)
+			command_execute(*command, name);
+	}
 }
--- serie_3/app/command.c
+++ serie_3/app/command.c
@@ -2,8 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 #include "command.h"
+#include <dlfcn.h>
+#include <errno.h>
 
-void commands_list_init(Commands *commands_list) {
+static Commands *commands_list;
+
+
+void commands_list_init() {
+	commands_list = malloc(sizeof *commands_list);
 	commands_list->commands = list_create();
 	commands_list->total = 0;
 }
@@ -15,7 +21,7 @@ void print_command(void *command) {
 	printf("Description: %s\n", data->description);
 }
 
-void print_commands(Commands *commands_list) {
+void print_commands() {
 	printf("Número de comandos = %d\n", commands_list->total);
 	if(commands_list->total > 0)
 		list_foreach(commands_list->commands, print_command);
@@ -28,7 +34,7 @@ int cmp_letter_command(void *item, void *letter) {
 	return ((Command *)data)->letter == *((char *) letter);
 }
 
-void command_insert(Commands *commands_list, char letter, char *description, void *command) {
+void command_insert(char letter, char *description, void *command) {
 	if(list_find(commands_list->commands, cmp_letter_command, &letter) != NULL) {
 		fprintf(stderr, "Command with letter [%c] already exists!\n", letter);
 	}
@@ -37,7 +43,7 @@ void command_insert(Commands *commands_list, char letter, char *description, voi
 	new_command->letter = letter;
 	new_command->description = malloc(strlen(description) + 1);
 	memmove((void *)new_command->description, description, strlen(description) + 1);
-	new_command->command = command;
+	new_command->func = command;
 	list_insert_front(commands_list->commands, new_command);
 	commands_list->total += 1;
 }
@@ -48,7 +54,7 @@ void command_delete(void *command) {
 	free(command);
 }
 
-void product_remove(Commands *commands_list, char letter) {
+void product_remove(char letter) {
 	Node *command = list_find(commands_list->commands, cmp_letter_command, &letter);
 	if(command != NULL) {
 		list_remove(command);
@@ -59,7 +65,77 @@ void product_remove(Commands *commands_list, char letter) {
 		fprintf(stderr, "Command with letter [%c] does not exist\n", letter);
 }
 
-void products_list_delete(Commands *commands_list) {
+void products_list_delete() {
 	list_destroy(commands_list->commands, command_delete);
 	free(commands_list);
 }
+
+// Start...
+//static Command *commands = NULL;
+
+void command_execute(char letter, char *param) {
+	Node *command_node = list_find(commands_list->commands, cmp_letter_command, &letter);
+	
+	if (command_node == NULL) return;
+	
+	Command *cmd = (Command *) (command_node->data);
+	
+	void (*func) (void *) =  *cmd->func;
+	
+	func(param);
+	
+	return;
+		
+}
+
+//static void *handle;
+
+void command_new(char *lib) {
+	void *handle = dlopen(lib, RTLD_LAZY);
+	
+	if (handle == NULL) {
+		fprintf(stderr, "%s\n", dlerror());
+		return;
+	}
+	
+	void (*func) (void *) = dlsym(handle, "command_function");
+	if (func == NULL) {
+		fprintf(stderr, "%s\n", dlerror());
+		return;
+	}
+	char *letter = dlsym(handle, "command_letter");
+	if (letter == NULL) {
+		fprintf(stderr, "%s\n", dlerror());
+		return;
+	}
+	char **description = dlsym(handle, "command_description");
+	if (description == NULL) {
+		fprintf(stderr, "%s\n", dlerror());
+		return;
+	}
+	command_insert(*letter, *description, *func);
+}
+
+
+/*
+void leave_program(char *unused) {
+	void *next;
+	for (User *p = queue.next; p != &queue; p = next) {
+		next = p->next;
+		p->prev->next = p->next;
+		p->next->prev = p->prev;
+		free(p->name);
+		free(p);
+	}
+	for (Command *p = commands; p != NULL; p = next) {
+		next = p->next;
+		free(p->desc);
+		free(p);
+	}
+	if (handle != NULL)
+		dlclose(handle);
+	exit(0);
+}*/
+
+
+
