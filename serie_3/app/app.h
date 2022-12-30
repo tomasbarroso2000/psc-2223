6fa8539650ad01c4775802681d2152ccc482f670
joseam10 <A48349@alunos.isel.pt>
2022-12-30 17:08:06 +0000

    App .c and .h in progress

--- serie_3/app/app.h
+++ serie_3/app/app.h
@@ -25,14 +25,12 @@ void print_data(void *data);
 
 void print_datalist(Datalist *data_list);
 
-int cmp_id_data(void *item, void *id);
+int cmp_user_data(void *item, void *user);
 
-void data_insert(Datalist *data_list, int user_id, size_t n_products, void *prods);
+void data_insert(Datalist *data_list, User *user, size_t n_products, void *prods);
 
 void data_delete(void *data);
 
-void data_remove(Datalist *data_list, int user_id);
+void data_remove(Datalist *data_list, User *user);
 
 void data_list_delete(Datalist *data_list);
-
-Datalist *data_list_get();
